698d67dc53988858bb730921d34efe974f9292d4
MaciejDziuban <dziuban.maciej@gmail.com>
2020-04-28 21:51:57 +0200

    Delete legacy MPI directory

--- MPI/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mpic++ -c -o node.o node/node.cpp -lX11
-mpic++ -c -o master.o master/master.cpp -lX11
-mpic++ -c -o main.o main.cpp -lX11
-mpic++ -o WSim main.o node.o master.o -lX11
--- MPI/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-mpirun -np 9 ./WSim
-#np must be a cube number+1 e.g. 2,9,28
-# 1 master + cube of nodes
