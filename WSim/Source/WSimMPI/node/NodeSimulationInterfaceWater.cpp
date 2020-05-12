01ada2bd66a3f73be051e3b7c3558b653fa89138
domin92 <intunek2@interia.pl>
2020-05-13 01:22:44 +0200

    Correct input buffers

--- WSim/Source/WSimMPI/node/NodeSimulationInterfaceWater.cpp
+++ WSim/Source/WSimMPI/node/NodeSimulationInterfaceWater.cpp
@@ -78,36 +78,36 @@ void NodeSimulationInterfaceWater::iter() {
 void NodeSimulationInterfaceWater::postShareCopy() {
     const auto &shareBuffers = node.get_share_buffers();
     const auto copy = [&shareBuffers, this](cl_mem image, size_t imageIndex) {
-        copier.postShareCopySide(image, imageIndex, shareBuffers.sh_vertical_D_out, Dim::Y, End::Higher);
-        copier.postShareCopySide(image, imageIndex, shareBuffers.sh_vertical_U_out, Dim::Y, End::Lower);
-        copier.postShareCopySide(image, imageIndex, shareBuffers.sh_horizontal_R_out, Dim::X, End::Higher);
-        copier.postShareCopySide(image, imageIndex, shareBuffers.sh_horizontal_L_out, Dim::X, End::Lower);
-        copier.postShareCopySide(image, imageIndex, shareBuffers.sh_depth_B_out, Dim::Z, End::Higher);
-        copier.postShareCopySide(image, imageIndex, shareBuffers.sh_depth_F_out, Dim::Z, End::Lower);
+        copier.postShareCopySide(image, imageIndex, shareBuffers.sh_vertical_D_in, Dim::Y, End::Higher);
+        copier.postShareCopySide(image, imageIndex, shareBuffers.sh_vertical_U_in, Dim::Y, End::Lower);
+        copier.postShareCopySide(image, imageIndex, shareBuffers.sh_horizontal_R_in, Dim::X, End::Higher);
+        copier.postShareCopySide(image, imageIndex, shareBuffers.sh_horizontal_L_in, Dim::X, End::Lower);
+        copier.postShareCopySide(image, imageIndex, shareBuffers.sh_depth_B_in, Dim::Z, End::Higher);
+        copier.postShareCopySide(image, imageIndex, shareBuffers.sh_depth_F_in, Dim::Z, End::Lower);
 
-        copier.postShareCopyCorner(image, imageIndex, shareBuffers.sh_corner_FUL_out, End::Lower, End::Lower, End::Lower);
-        copier.postShareCopyCorner(image, imageIndex, shareBuffers.sh_corner_BDR_out, End::Higher, End::Higher, End::Higher);
-        copier.postShareCopyCorner(image, imageIndex, shareBuffers.sh_corner_FUR_out, End::Lower, End::Lower, End::Higher);
-        copier.postShareCopyCorner(image, imageIndex, shareBuffers.sh_corner_BDL_out, End::Higher, End::Higher, End::Lower);
-        copier.postShareCopyCorner(image, imageIndex, shareBuffers.sh_corner_FDL_out, End::Lower, End::Higher, End::Lower);
-        copier.postShareCopyCorner(image, imageIndex, shareBuffers.sh_corner_BUR_out, End::Higher, End::Lower, End::Higher);
-        copier.postShareCopyCorner(image, imageIndex, shareBuffers.sh_corner_FDR_out, End::Lower, End::Higher, End::Higher);
-        copier.postShareCopyCorner(image, imageIndex, shareBuffers.sh_corner_BUL_out, End::Higher, End::Lower, End::Lower);
+        copier.postShareCopyCorner(image, imageIndex, shareBuffers.sh_corner_FUL_in, End::Lower, End::Lower, End::Lower);
+        copier.postShareCopyCorner(image, imageIndex, shareBuffers.sh_corner_BDR_in, End::Higher, End::Higher, End::Higher);
+        copier.postShareCopyCorner(image, imageIndex, shareBuffers.sh_corner_FUR_in, End::Lower, End::Lower, End::Higher);
+        copier.postShareCopyCorner(image, imageIndex, shareBuffers.sh_corner_BDL_in, End::Higher, End::Higher, End::Lower);
+        copier.postShareCopyCorner(image, imageIndex, shareBuffers.sh_corner_FDL_in, End::Lower, End::Higher, End::Lower);
+        copier.postShareCopyCorner(image, imageIndex, shareBuffers.sh_corner_BUR_in, End::Higher, End::Lower, End::Higher);
+        copier.postShareCopyCorner(image, imageIndex, shareBuffers.sh_corner_FDR_in, End::Lower, End::Higher, End::Higher);
+        copier.postShareCopyCorner(image, imageIndex, shareBuffers.sh_corner_BUL_in, End::Higher, End::Lower, End::Lower);
 
-        copier.postShareCopyEdge(image, imageIndex, shareBuffers.sh_edge_UL_out, Dim::Y, End::Lower, Dim::X, End::Lower);
-        copier.postShareCopyEdge(image, imageIndex, shareBuffers.sh_edge_DR_out, Dim::Y, End::Higher, Dim::X, End::Higher);
-        copier.postShareCopyEdge(image, imageIndex, shareBuffers.sh_edge_UR_out, Dim::Y, End::Lower, Dim::X, End::Higher);
-        copier.postShareCopyEdge(image, imageIndex, shareBuffers.sh_edge_DL_out, Dim::Y, End::Higher, Dim::X, End::Lower);
+        copier.postShareCopyEdge(image, imageIndex, shareBuffers.sh_edge_UL_in, Dim::Y, End::Lower, Dim::X, End::Lower);
+        copier.postShareCopyEdge(image, imageIndex, shareBuffers.sh_edge_DR_in, Dim::Y, End::Higher, Dim::X, End::Higher);
+        copier.postShareCopyEdge(image, imageIndex, shareBuffers.sh_edge_UR_in, Dim::Y, End::Lower, Dim::X, End::Higher);
+        copier.postShareCopyEdge(image, imageIndex, shareBuffers.sh_edge_DL_in, Dim::Y, End::Higher, Dim::X, End::Lower);
 
-        copier.postShareCopyEdge(image, imageIndex, shareBuffers.sh_edge_FL_out, Dim::Z, End::Lower, Dim::X, End::Lower);
-        copier.postShareCopyEdge(image, imageIndex, shareBuffers.sh_edge_BR_out, Dim::Z, End::Higher, Dim::X, End::Higher);
-        copier.postShareCopyEdge(image, imageIndex, shareBuffers.sh_edge_FR_out, Dim::Z, End::Lower, Dim::X, End::Higher);
-        copier.postShareCopyEdge(image, imageIndex, shareBuffers.sh_edge_BL_out, Dim::Z, End::Higher, Dim::X, End::Lower);
+        copier.postShareCopyEdge(image, imageIndex, shareBuffers.sh_edge_FL_in, Dim::Z, End::Lower, Dim::X, End::Lower);
+        copier.postShareCopyEdge(image, imageIndex, shareBuffers.sh_edge_BR_in, Dim::Z, End::Higher, Dim::X, End::Higher);
+        copier.postShareCopyEdge(image, imageIndex, shareBuffers.sh_edge_FR_in, Dim::Z, End::Lower, Dim::X, End::Higher);
+        copier.postShareCopyEdge(image, imageIndex, shareBuffers.sh_edge_BL_in, Dim::Z, End::Higher, Dim::X, End::Lower);
 
-        copier.postShareCopyEdge(image, imageIndex, shareBuffers.sh_edge_FU_out, Dim::Z, End::Lower, Dim::Y, End::Lower);
-        copier.postShareCopyEdge(image, imageIndex, shareBuffers.sh_edge_BD_out, Dim::Z, End::Higher, Dim::Y, End::Higher);
-        copier.postShareCopyEdge(image, imageIndex, shareBuffers.sh_edge_FD_out, Dim::Z, End::Lower, Dim::Y, End::Higher);
-        copier.postShareCopyEdge(image, imageIndex, shareBuffers.sh_edge_BU_out, Dim::Z, End::Higher, Dim::Y, End::Lower);
+        copier.postShareCopyEdge(image, imageIndex, shareBuffers.sh_edge_FU_in, Dim::Z, End::Lower, Dim::Y, End::Lower);
+        copier.postShareCopyEdge(image, imageIndex, shareBuffers.sh_edge_BD_in, Dim::Z, End::Higher, Dim::Y, End::Higher);
+        copier.postShareCopyEdge(image, imageIndex, shareBuffers.sh_edge_FD_in, Dim::Z, End::Lower, Dim::Y, End::Higher);
+        copier.postShareCopyEdge(image, imageIndex, shareBuffers.sh_edge_BU_in, Dim::Z, End::Higher, Dim::Y, End::Lower);
     };
 
     copy(simulation.getColor().getSource(), 0);
