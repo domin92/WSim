c4ae4c8dc886e4dcf70815ab05cc3ffc7ca85062
Rafał Jaśniak <jasniakrafal@gmail.com>
2020-05-13 22:04:01 +0200

    height

--- WSim/Source/WSimRenderer/main.cpp
+++ WSim/Source/WSimRenderer/main.cpp
@@ -9,6 +9,9 @@
 #include <glm/gtc/type_ptr.hpp>
 // clang-format on
 
+#define WINDOW_WIDTH 1280
+#define WINDOWHEIGHT 720
+
 void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
     glViewport(0, 0, width, height);
 }
@@ -32,7 +35,7 @@ int rendererMain() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow *window = glfwCreateWindow(800, 800, "LearnOpenGL", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(WINDOW_WIDTH, WINDOWHEIGHT, "LearnOpenGL", NULL, NULL);
     if (window == NULL) {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
