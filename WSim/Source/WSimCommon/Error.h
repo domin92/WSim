bf2384c09e2738a068b524438ee6eab6f04bec4f
MaciejDziuban <dziuban.maciej@gmail.com>
2020-06-27 01:39:24 +0200

    Change wsimError functions to macros so we can use __FILE__ and __LINE__

--- WSim/Source/WSimCommon/Error.h
+++ WSim/Source/WSimCommon/Error.h
@@ -4,6 +4,7 @@
 
 #include <cstdlib>
 
+#ifdef WSIM_ERROR_AS_FUNCTIONS
 [[noreturn]] inline void wsimError() {
     Logger::get() << "wsimError called!\n";
     std::abort();
@@ -20,3 +21,22 @@ inline void wsimErrorUnless(bool condition) {
         wsimError();
     }
 }
+#else
+#define wsimError()                                                                 \
+    Logger::get() << "wsimError called at " << __FILE__ << ":" << __LINE__ << '\n'; \
+    std::abort();
+
+#define wsimErrorIf(condition) \
+    {                          \
+        if (condition) {       \
+            wsimError();       \
+        }                      \
+    }
+
+#define wsimErrorUnless(condition) \
+    {                              \
+        if (!(condition)) {        \
+            wsimError();           \
+        }                          \
+    }
+#endif
