698ec6b00de0216b83f386070e76ec0bad51b3a6
MaciejDziuban <dziuban.maciej@gmail.com>
2020-10-28 21:33:38 +0100

    Update shader - use sign of the levelset field

--- WSim/Source/WSimRenderer/Shaders/Fragment/LevelSetFragment.glsl
+++ WSim/Source/WSimRenderer/Shaders/Fragment/LevelSetFragment.glsl
@@ -3,33 +3,14 @@
 in vec3 worldPos;
 
 uniform sampler3D volume;
-uniform int nodeSize;
 uniform vec3 cameraPosition;
-
-uniform ivec3 volume_dims;
 
 out vec4 color;
 
-vec3 getWaterTextureCoords(vec3 readPos) {
-    int gridSize = 1;
-
-    // This function converts world space coorinates to water texture coordinates. It is needed to properly read multi-node textures.
-
-    int z_in_grid = int(readPos.z * float(gridSize));
-    int y_in_grid = int(readPos.y * float(gridSize));
-    int x_in_grid = int(readPos.x * float(gridSize));
-
-    float nodeOffset = float(z_in_grid * gridSize * gridSize + y_in_grid * gridSize + x_in_grid) / float(gridSize * gridSize * gridSize);
-
-    vec3 waterCoords = readPos;
-
-    waterCoords.x = (readPos.x - float(x_in_grid) / float(gridSize)) * float(gridSize);
-    waterCoords.y = (readPos.y - float(y_in_grid) / float(gridSize)) * float(gridSize);
-    waterCoords.z = (readPos.z - float(z_in_grid) / float(gridSize)) * float(gridSize);
-
-    waterCoords.z = nodeOffset + waterCoords.z / float(gridSize * gridSize * gridSize);
-
-    return waterCoords;
+bool isSamplePositionValid(vec3 samplePosition) {
+    return samplePosition.x >= 0 && samplePosition.x <= 1
+        && samplePosition.y >= 0 && samplePosition.y <= 1
+        && samplePosition.z >= 0 && samplePosition.z <= 1;
 }
 
 void main(void) {
@@ -37,27 +18,27 @@ void main(void) {
     marchingRay = normalize(marchingRay);
     vec3 step = marchingRay / 50;
     vec3 samplePosition = worldPos;
-    float val = 0;
-    color = vec4(0.0f, 0.0f, 0.0f, 1.0f);
-    bool found = false;
+
+    // First step
+    float levelSetSign = sign(texture(volume, samplePosition).r);
+    samplePosition += step;
+
+    // Next steps
     for (float i = 0; i < 1000; i++) {
-        if ((samplePosition.x < 0) || (samplePosition.y < 0) || (samplePosition.z < 0)) {
-            break;
-        }
-        if ((samplePosition.x > 1) || (samplePosition.y > 1) || (samplePosition.z > 1)) {
+        if (!isSamplePositionValid(samplePosition)) {
             break;
         }
 
-        val = texture(volume, samplePosition).r;
-        if (val < 0.1f) {
-            color = vec4(0.0f, 0.0f, 1.0f, 1.0f);
-            found = true;
-            break;
+        float currentSign = sign(texture(volume, samplePosition).r);
+        if (levelSetSign != currentSign) {
+            color = vec4(0.0f, 0.0f, 1.f, 1.0f);
+            return;
         }
-        samplePosition = samplePosition + step;
+
+        levelSetSign = currentSign;
+        samplePosition += step;
     }
-    if (!found) {
-        color = vec4(1.0f, 0.0f, 0.0f, 1.0f);
-        //discard;
-    }
+
+    // Not found water
+    color = vec4(1.0f, 0.0f, 0.0f, 1.0f);
 }
